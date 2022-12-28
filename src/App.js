84355b58797f76f2110f8f3223be2c3e1ea060ae
Vladimir RM <shvdrummer@gmail.com>
2022-12-28 19:33:15 +0200

    create main div and button

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+<div className="quiz">
+  <div className="question-section">
+    <div className="question-count">
+      <span>Question 1</span> / 4
+
+    </div>
+    <div className="question-text"> Text Question 1</div>
+  </div>
+  <div className="answer-section">
+    <button>variant-1</button>
+    <button>variant-2</button>
+    <button>variant-3</button>
+    <button>variant-4</button>
+  </div>
+</div>
+   
+    </div>
+  );
+}
+
+export default App;
