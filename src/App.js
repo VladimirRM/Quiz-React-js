bd9c2c03d6a708d13ea4c8028554dc2f32d3fbed
Vladimir RM <shvdrummer@gmail.com>
2022-12-29 07:58:22 +0200

    change obj in arr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const questions = [
+    {
+      questionText: "Capital of USA?",
+      answerOptions: [
+        { answerText: "Boston", isCorrect: false },
+        { answerText: "Washington", isCorrect: true },
+        { answerText: "New-York", isCorrect: false },
+        { answerText: "Los-Angeles", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is not a programming language?",
+      answerOptions: [
+        { answerText: "Go", isCorrect: false },
+        { answerText: "HTML", isCorrect: true },
+        { answerText: "Java-script", isCorrect: false },
+        { answerText: "Python", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Which company developed React?",
+      answerOptions: [
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Mail", isCorrect: false },
+        { answerText: "Facebook", isCorrect: true },
+        { answerText: "Google", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What does not apply to the universe Marvel?",
+      answerOptions: [
+        { answerText: "Batman", isCorrect: true },
+        { answerText: "Hulk", isCorrect: false },
+        { answerText: "Iron-Man", isCorrect: false },
+        { answerText: "Avengers", isCorrect: false },
+      ],
+    },
+  ];
+
+  const [currentQuestions, setCurrentQuestions] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestions + 1;
+
+    if (nextQuestion < questions.length) {
+      setCurrentQuestions(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
     <div className="app">
-<div className="quiz">
-  <div className="question-section">
-    <div className="question-count">
-      <span>Question 1</span> / 4
-
-    </div>
-    <div className="question-text"> Text Question 1</div>
-  </div>
-  <div className="answer-section">
-    <button>variant-1</button>
-    <button>variant-2</button>
-    <button>variant-3</button>
-    <button>variant-4</button>
-  </div>
-</div>
-   
+      <div className="quiz">
+        <div className="question-section">
+          <div className="question-count">
+            <span>Question {currentQuestions + 1}</span> / {questions.length}
+          </div>
+          <div className="question-text">
+            {questions[currentQuestions].questionText}
+          </div>
+        </div>
+        <div className="answer-section">
+          {questions[currentQuestions].answerOptions.map((item) => (
+            <button onClick={() => handleAnswerOptionClick(item.isCorrect)}>
+              {item.answerText}
+            </button>
+          ))}
+          ;
+        </div>
+      </div>
     </div>
   );
 }
