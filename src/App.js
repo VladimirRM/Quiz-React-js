55003bbc9933afaa6b6f597b9ec213a9a7a8a7b9
Vladimir RM <shvdrummer@gmail.com>
2022-12-31 09:40:36 +0200

    add main arr questions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
+
+const questions = [
+  {
+    questionText: "What a capital of USA ?",
+    answerOptions: [
+      { answerText: "Boston", isCorrect: false },
+      { answerText: "Washington", isCorrect: true },
+      { answerText: "New-york", isCorrect: false },
+      { answerText: "LOs-Angeles", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is not a programming language?",
+    answerOptions: [
+      { answerText: "Go", isCorrect: false },
+      { answerText: "HTML", isCorrect: true },
+      { answerText: "Java-script", isCorrect: false },
+      { answerText: "Python", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which company developed React?",
+    answerOptions: [
+      { answerText: "Amazon", isCorrect: false },
+      { answerText: "Mail", isCorrect: false },
+      { answerText: "Facebook", isCorrect: true },
+      { answerText: "Google", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What does not apply to the universe Marvel?",
+    answerOptions: [
+      { answerText: "Batman", isCorrect: true },
+      { answerText: "Hulk", isCorrect: false },
+      { answerText: "Iron-Man", isCorrect: false },
+      { answerText: "Avengers", isCorrect: false },
+    ],
+  },
+];
+
+const [currentQuestions, setCurrentQuestions]= useState( )
 
 const App = () => {
   return (
