f11088af705bc6e5e23ce478789276f5395a286b
Vladimir RM <shvdrummer@gmail.com>
2022-12-31 08:15:36 +0200

    add duble file

---
+++ src/app2.js
@@ -0,0 +1,93 @@
+// import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const questions = [
+    {
+      questionText: "Capital of USA?",
+      answerOptions: [
+        { answerText: "Boston", isCorrect: false },
+        { answerText: "Washington", isCorrect: true },
+        { answerText: "New-York", isCorrect: false },
+        { answerText: "Los-Angeles", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is not a programming language?",
+      answerOptions: [
+        { answerText: "Go", isCorrect: false },
+        { answerText: "HTML", isCorrect: true },
+        { answerText: "Java-script", isCorrect: false },
+        { answerText: "Python", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Which company developed React?",
+      answerOptions: [
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Mail", isCorrect: false },
+        { answerText: "Facebook", isCorrect: true },
+        { answerText: "Google", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What does not apply to the universe Marvel?",
+      answerOptions: [
+        { answerText: "Batman", isCorrect: true },
+        { answerText: "Hulk", isCorrect: false },
+        { answerText: "Iron-Man", isCorrect: false },
+        { answerText: "Avengers", isCorrect: false },
+      ],
+    },
+  ];
+
+  const [currentQuestions, setCurrentQuestions] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestions + 1;
+
+    if (nextQuestion < questions.length) {
+      setCurrentQuestions(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div className="app">
+      {showScore ? (
+        <div className="section-score">
+          <div>
+            The right answer {score} from {questions.length}
+          </div>
+        </div>
+      ) : (
+        <div className="quiz">
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestions + 1}</span> / {questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestions].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestions].answerOptions.map((item) => (
+              <button onClick={() => handleAnswerOptionClick(item.isCorrect)}>
+                {item.answerText}
+              </button>
+            ))}
+            ;
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+// export default App;
